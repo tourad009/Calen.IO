498cd117d9f110a1a6e2902971b5dd1a91fccb6f
soulty <ouattarame380@gmail.com>
2024-12-09 20:20:43 +0000

    chore: ajout de l'application task

--- back/backend/backend/settings.py
+++ back/backend/backend/settings.py
@@ -76,10 +76,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+        "NAME": "calendar",
+        "ENGINE": "django.db.backends.postgresql",
+        "USER": "postgres",
+        "PASSWORD": "passer",
+    },
 }
 
 
--- back/backend/user_app/models.py
+++ back/backend/user_app/models.py
@@ -3,5 +3,4 @@ from django.db import models
 # Create your models here.
 class user(models.Model):
     username = models.charfields(max_length = 10)
-    # password
-    
+    password = models.CharField(max_length=50)
