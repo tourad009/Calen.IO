02d4dcc6a0d6fc2e985083f7b740445f26c6797f
soulty <ouattarame380@gmail.com>
2024-12-09 20:44:59 +0000

    fix: correction de la base de donnee\nchore: migration de la base de donees

--- back/backend/backend/urls.py
+++ back/backend/backend/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import user_app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user_app.urls')),
+    path('', include('task.urls')),
+
 ]
--- back/backend/user_app/models.py
+++ back/backend/user_app/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class user(models.Model):
-    username = models.charfields(max_length = 10)
+    username = models.CharField(max_length = 10)
     password = models.CharField(max_length=50)
