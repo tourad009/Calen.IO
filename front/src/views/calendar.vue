215dce765a71188f0c225dc5d9716e8df09173a0
Djibril Dia <154755745+DjibrilDia1@users.noreply.github.com>
2024-12-10 19:03:56 +0000

    Configuration de vue-router et implementation de login.vue

--- front/src/views/calendar.vue
+++ front/src/views/calendar.vue
@@ -4,19 +4,24 @@ import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
-import { INITIAL_EVENTS, createEventId } from './event-utils'
+import { INITIAL_EVENTS, createEventId } from '../event-utils'
+import Popup from '../components/popup.vue'
 
 export default defineComponent({
   components: {
     FullCalendar,
+    Popup,
   },
   data() {
     return {
+      showPopup: false, // gerer la visibilité du popup
+      newEventTitle: '', // Stocke le titre du nouvel événement
+      selectedDates: null, // Stocke les informations de la sélection de dates
       calendarOptions: {
         plugins: [
           dayGridPlugin,
           timeGridPlugin,
-          interactionPlugin // needed for dateClick
+          interactionPlugin // nécessaire pour gérer les clics sur le calendrier
         ],
         headerToolbar: {
           left: 'prev,next today',
@@ -24,101 +29,109 @@ export default defineComponent({
           right: 'dayGridMonth,timeGridWeek,timeGridDay'
         },
         initialView: 'dayGridMonth',
-        initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
+        initialEvents: INITIAL_EVENTS, // évènements initiaux
         editable: true,
         selectable: true,
         selectMirror: true,
         dayMaxEvents: true,
         weekends: true,
-        select: this.handleDateSelect,
+        select: this.handleDateSelect, // Gère la sélection des dates
         eventClick: this.handleEventClick,
-        eventsSet: this.handleEvents
-        /* you can update a remote database when these fire:
-        eventAdd:
-        eventChange:
-        eventRemove:
-        */
+        eventsSet: this.handleEvents,
       },
-      currentEvents: [],
+      currentEvents: [], // Liste des événements actuels
     }
   },
   methods: {
-    handleWeekendsToggle() {
-      this.calendarOptions.weekends = !this.calendarOptions.weekends // update a property
-    },
     handleDateSelect(selectInfo) {
-      let title = prompt('Please enter a new title for your event')
-      let calendarApi = selectInfo.view.calendar
+      // Enregistre les informations des dates sélectionnées et affiche le popup
+      this.selectedDates = selectInfo;
+      this.showPopup = true;
+    },
+    closePopup() {
+      this.showPopup = false; // Ferme le popup
+      this.newEventTitle = ''; // Réinitialise le titre
+    },
+    saveEvent() {
+      // Ajoute un nouvel événement basé sur les informations du popup
+      const calendarApi = this.selectedDates.view.calendar;
+      calendarApi.unselect(); // Efface la sélection des dates dans le calendrier
 
-      calendarApi.unselect() // clear date selection
-
-      if (title) {
+      if (this.newEventTitle) {
         calendarApi.addEvent({
           id: createEventId(),
-          title,
-          start: selectInfo.startStr,
-          end: selectInfo.endStr,
-          allDay: selectInfo.allDay
-        })
+          title: this.newEventTitle,
+          start: this.selectedDates.startStr,
+          end: this.selectedDates.endStr,
+          allDay: this.selectedDates.allDay,
+        });
       }
+      this.closePopup(); // Ferme le popup après l'enregistrement
     },
     handleEventClick(clickInfo) {
       if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-        clickInfo.event.remove()
+        clickInfo.event.remove();
       }
     },
     handleEvents(events) {
-      this.currentEvents = events
+      this.currentEvents = events;
     },
-  }
-})
-
+  },
+});
 </script>
-
 <template>
-  <div class='demo-app'>
-    <div class='demo-app-sidebar'>
-      <div class='demo-app-sidebar-section'>
+  <div class="demo-app">
+    <div class="demo-app-sidebar">
+      <div class="demo-app-sidebar-section">
         <h2>Instructions</h2>
         <ul>
           <li>Select dates and you will be prompted to create a new event</li>
           <li>Drag, drop, and resize events</li>
-          <li>Click an event to delete it</li>
+          <li>Click an event to edit or delete it</li>
         </ul>
       </div>
-      <div class='demo-app-sidebar-section'>
+      <div class="demo-app-sidebar-section">
         <label>
           <input
-            type='checkbox'
-            :checked='calendarOptions.weekends'
-            @change='handleWeekendsToggle'
+            type="checkbox"
+            :checked="calendarOptions.weekends"
+            @change="calendarOptions.weekends = !calendarOptions.weekends"
           />
-          toggle weekends
+          Toggle weekends
         </label>
       </div>
-      <div class='demo-app-sidebar-section'>
+      <div class="demo-app-sidebar-section">
         <h2>All Events ({{ currentEvents.length }})</h2>
         <ul>
-          <li v-for='event in currentEvents' :key='event.id'>
+          <li v-for="event in currentEvents" :key="event.id">
             <b>{{ event.startStr }}</b>
             <i>{{ event.title }}</i>
           </li>
         </ul>
       </div>
     </div>
-    <div class='demo-app-main'>
+    <div class="demo-app-main">
       <FullCalendar
-        class='demo-app-calendar'
-        :options='calendarOptions'
+        ref="calendar"
+        class="demo-app-calendar"
+        :options="calendarOptions"
       >
-        <template v-slot:eventContent='arg'>
+        <template v-slot:eventContent="arg">
           <b>{{ arg.timeText }}</b>
           <i>{{ arg.event.title }}</i>
         </template>
       </FullCalendar>
     </div>
+    <!-- Popup -->
+    <Popup
+      :show="showPopup"
+      @close="closePopup"
+      @save="saveEvent"
+      :event="null"
+    />
   </div>
 </template>
+
 
 <style lang='css'>
 
---
+++ front/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'], // Ajustez les chemins selon votre projet
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
